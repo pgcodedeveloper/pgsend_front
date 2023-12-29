d501da7d9a5677aafa826d4ef31f767a47ea6ed4
Pablo Emanuel Gillespie <103851582+pgcodedeveloper@users.noreply.github.com>
2023-12-29 16:19:37 -0300

    Cambios de env

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        backendURL: 'http://localhost:4000',
-        frontendURL: 'http://localhost:3000'
-    }
 }
 
 module.exports = nextConfig
